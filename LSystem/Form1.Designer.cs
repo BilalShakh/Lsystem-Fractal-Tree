821d48bf72effee662f87ce464b5f86b690f2220
BilalShakh <bilalzaki92@gmail.com>
2019-06-28 20:33:47 -0400

    Final

---
+++ LSystem/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace LSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LSystemPictureBox = new System.Windows.Forms.PictureBox();
+            this.GenrateButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.InstructionsTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.LSystemPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LSystemPictureBox
+            // 
+            this.LSystemPictureBox.BackColor = System.Drawing.Color.DimGray;
+            this.LSystemPictureBox.Location = new System.Drawing.Point(13, 13);
+            this.LSystemPictureBox.Name = "LSystemPictureBox";
+            this.LSystemPictureBox.Size = new System.Drawing.Size(400, 400);
+            this.LSystemPictureBox.TabIndex = 0;
+            this.LSystemPictureBox.TabStop = false;
+            this.LSystemPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.LSystemPictureBox_Paint);
+            // 
+            // GenrateButton
+            // 
+            this.GenrateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenrateButton.Location = new System.Drawing.Point(13, 420);
+            this.GenrateButton.Name = "GenrateButton";
+            this.GenrateButton.Size = new System.Drawing.Size(112, 36);
+            this.GenrateButton.TabIndex = 1;
+            this.GenrateButton.Text = "Generate";
+            this.GenrateButton.UseVisualStyleBackColor = true;
+            this.GenrateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetButton.Location = new System.Drawing.Point(322, 420);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(91, 36);
+            this.ResetButton.TabIndex = 3;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // InstructionsTextbox
+            // 
+            this.InstructionsTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InstructionsTextbox.Location = new System.Drawing.Point(447, 13);
+            this.InstructionsTextbox.Multiline = true;
+            this.InstructionsTextbox.Name = "InstructionsTextbox";
+            this.InstructionsTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InstructionsTextbox.Size = new System.Drawing.Size(480, 400);
+            this.InstructionsTextbox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(447, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Instructions followed by the program to create the image.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 479);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InstructionsTextbox);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.GenrateButton);
+            this.Controls.Add(this.LSystemPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.LSystemPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox LSystemPictureBox;
+        private System.Windows.Forms.Button GenrateButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.TextBox InstructionsTextbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LSystem/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LSystem
+{
+    public partial class Form1 : Form
+    {
+        //All global variables used in this program.
+        private static string axiom = "F";
+        private static float length = 75.0f;
+        private static Rule makeBranches = new Rule("F", "FF+[+F-F-F]-[-F+F+F]");
+
+        private static string LSystemString;
+        private static Bitmap LSystemBitmap = new Bitmap(400, 400);
+        private static Graphics LSystemDrawingCanvas;
+        private static Pen WhitePen = new Pen(Color.White);
+
+        //struct used to create Rules for the Lsystem string creation.
+        private struct Rule
+        {
+            public string A, B;
+
+            public Rule(string a, string b)
+            {
+                A = a;
+                B = b;
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LSystemString = axiom;
+            LSystemDrawingCanvas = Graphics.FromImage(LSystemBitmap);
+        }
+
+        //Event raised for changing the paintbox using the Bitmap.
+        private void LSystemPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            //SaveRestore1(e);
+            e.Graphics.DrawImage(LSystemBitmap, 0, 0);
+        }
+
+        //Event for when the user presses the Generate Button.
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+
+            length *= 0.56f;
+
+            //To prevent the user from causing the program to create too small lines and go through very long instructions.
+            if (length > 4)
+            {
+                //Initialize transformation state
+                LSystemDrawingCanvas.ResetTransform();
+                LSystemDrawingCanvas.TranslateTransform(200, 400);
+
+                LSystemString = GenerateLSystemString(LSystemString);
+                Turtle(LSystemString);
+                InstructionsTextbox.Text = LSystemString;
+            }
+            else
+                MessageBox.Show("Lines are too small to show","Error");
+
+        }
+
+        //Generates the string which has the instructions for the turtle method to follow.
+        private static string GenerateLSystemString(string currentLSystemString)
+        {
+            string NewLSystemString = "";
+
+            for (int i = 0; i < currentLSystemString.Length; i++)
+            {
+                string CurrentSymbol = currentLSystemString.Substring(i, 1);
+
+                if (CurrentSymbol == makeBranches.A)
+                {
+                    NewLSystemString += makeBranches.B;
+                }
+                else
+                {
+                    NewLSystemString += CurrentSymbol;
+                }
+            }
+
+            return NewLSystemString;
+        }
+
+        //The turtle which executes the instructions encoded in the LSystemString created by GenerateLSystem.
+        private void Turtle(string LSystemString)
+        {
+            //Stack which holds all the saved Information.
+            List<GraphicsState> transformationStateStack = new List<GraphicsState>();
+            
+            //Loop for checking the whole string, letter by letter and execute the instructions associated with it.
+            for (int i = 0; i < LSystemString.Length; i++)
+            {
+                string CurrentSymbol = LSystemString.Substring(i, 1);
+
+                if (CurrentSymbol == "F")
+                {
+                    LSystemDrawingCanvas.DrawLine(WhitePen, 0, 0, 0, -length);
+                    LSystemDrawingCanvas.TranslateTransform(0, -length);
+                }
+                else if (CurrentSymbol == "+")
+                {
+                    LSystemDrawingCanvas.RotateTransform(30);
+                }
+                else if (CurrentSymbol == "-")
+                {
+                    LSystemDrawingCanvas.RotateTransform(-30);
+                }
+                else if (CurrentSymbol == "[")
+                {
+                    Push(LSystemDrawingCanvas.Save(), transformationStateStack);
+                }
+                else if (CurrentSymbol == "]")
+                {
+                    LSystemDrawingCanvas.Restore(Pop(transformationStateStack));
+                }
+            }
+
+            //After all instructions have been executed refresh the PictureBox to start the paint event.
+            LSystemPictureBox.Refresh();
+        }
+
+        //Push data into a stack in the form of an item in a list. Essentially put something on top of a stack.
+        private static void Push(GraphicsState transformationState, List<GraphicsState> stack)
+        {
+            stack.Add(transformationState);
+        }
+
+        // A try...catch statement is used here just in case this method attempts to 
+        // "pop" an item from an empty stack.
+
+        //Pop out the last item out of a list, so what ever enters the List first is last out. 
+        private static GraphicsState Pop(List<GraphicsState> stack)
+        {
+            try
+            {
+                GraphicsState transformationState = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                return transformationState;
+            }
+            catch (System.IndexOutOfRangeException exception)
+            {
+                throw new System.ArgumentException("Cannot pop a value from an empty stack.", "stack", exception);
+            }
+        }
+
+        //Event for resetting the program.
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            LSystemString = axiom;
+            LSystemDrawingCanvas.ResetTransform();
+            LSystemDrawingCanvas.TranslateTransform(200, 400);
+            LSystemDrawingCanvas.Clear(LSystemPictureBox.BackColor);
+            LSystemPictureBox.Refresh();
+            InstructionsTextbox.Text = "";
+            length = 75.0f;
+        }
+    }
+}
